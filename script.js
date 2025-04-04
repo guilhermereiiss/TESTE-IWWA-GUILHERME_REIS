37a5a899ed5d78861eb032462c742e4b5d4c9418
guilhermereiiss <guiprogramador06@gmail.com>
2025-04-04 15:08:26 -0300

    responsividade v1.0

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const nav = document.querySelector(".nav");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("lista-doacoes");
 
     for (let i = 1; i <= 15; i++) {
@@ -67,7 +77,6 @@ for (let i = 0; i < 30; i++) {
 }
 
 const wrapperBox = document.getElementById('box-wrapper');
-
 for (let i = 0; i < 10; i++) {
     const boxItem = document.createElement('div');
     boxItem.className = 'grid-box';
